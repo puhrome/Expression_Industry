3f1f50f85098493e1824c85f4900461442be6b4a
puhrome <phirom.yim@fullsail.edu>
2014-04-13 19:31:52 -0400

    Expressions_Industry
    Using WebStorm to calculate average amount of hours I spend in front of
    a computer. Usage of strings and arrays for the expression.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Expressions Industry Created by Phirom Yim on 4/13/14.
+ */
+//How many hours a week do I spend on the computer?
+var hours = [5, 6, 4, 8,5, .5, 4.5, 7];//declaring and defining arrays for hours spent on computer
+var total =hours[0] + hours[1] + hours[2] + hours[3] + hours[4] + hours[5] + hours[6];//adding up arrays
+console.log(total);//print total
+var average = total/7;//average is total hours divided 7 days in a week
+var string= (" average hours spent on a computer each week");//assigning string variable to expression
+console.log(average + string);//print expression
+
